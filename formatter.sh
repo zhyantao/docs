a5538241f59b8da5b962281477621221135c08f6
zhyantao <yantao.z@outlook.com>
2024-10-23 00:31:41 +0800

    typo: format formatter.sh

--- formatter.sh
+++ formatter.sh
@@ -1,7 +1,7 @@
 pip install -r requirements.txt
 SITE_PACKAGES_DIR=$(python -c "import site; print(site.getsitepackages()[0]);")
 find . -type f \( -name "*.cpp" -o -name "*.c" -o -name "*.cc" -o -name "*.h" -o -name "*.hpp" \) \
--not -path "./.git/*" \
--not -path "./.svn/*" \
--not -path "./.github/*" \
--exec $SITE_PACKAGES_DIR/clang_format/data/bin/clang-format -i {} +
+    -not -path "./.git/*" \
+    -not -path "./.svn/*" \
+    -not -path "./.github/*" \
+    -exec $SITE_PACKAGES_DIR/clang_format/data/bin/clang-format -i {} +
