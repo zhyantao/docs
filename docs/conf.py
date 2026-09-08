7c18ff10e56cb4991adffa03ebb8a1dcf8ad789a
zhyantao <yantao.z@outlook.com>
2026-09-08 09:58:13 +0800

    Update copyright year to 2026

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ import os
 # -- Project information -----------------------------------------------------
 
 project = "Notebook"
-copyright = "2025"
+copyright = "2026"
 author = "Yantao Zhang (yantao.z [at] outlook.com)"
 master_doc = "index"
 language = "en"
